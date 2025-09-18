f901c9cab839182184ea157c35f379e9385a4c8f
blacklotusdev8 <blacklotusdev8@gmail.com>
2025-09-18 02:41:28 +0000

    it worke locl

--- src/hello_server/server.py
+++ src/hello_server/server.py
@@ -2,13 +2,13 @@ from mcp.server.fastmcp import Context, FastMCP
 from scrapling.fetchers import StealthyFetcher 
 from playwright.sync_api import Page
 from smithery.decorators import smithery
+import asyncio
 
 
 @smithery.server()
 def create_server():
     """Create and configure the MCP server."""
     server = FastMCP("Say Hello")
-
     @server.tool()
     def hello(name: str, ctx: Context) -> str:
         """Say hello to someone."""
@@ -18,15 +18,24 @@ def create_server():
     async def scrape(url: str, ctx: Context) -> str:
         """Scrape a website."""
         try:
+            loop = asyncio.get_running_loop()
+
             def scroll_page(page: Page):
                 page.mouse.wheel(10, 0)
                 page.mouse.move(100, 400)
                 page.mouse.up()
-
-            page = await StealthyFetcher.fetch(
-                url,
+                page.screenshot(path="example.png")
+                
+            def scrape_generate_image():
+                page =  StealthyFetcher.fetch(
+                url,solve_cloudflare=True,headless=True,
                 page_action=scroll_page
-            )
+                    )
+                
+                return page.get_all_text()
+
+            result = await loop.run_in_executor(None, scrape_generate_image)
+            return result
         except Exception as e:
             return str(e)
         return page.content    
